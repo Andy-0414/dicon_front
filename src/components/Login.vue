5792f869193f43786e7dc123a72084865c5c8ca0
Andy0414 <pjh8667@naver.com>
2018-11-15 00:00:15 +0900

    last login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,6 +6,7 @@
                 <span class="headline pl-3 pt-3">로그인</span>
             </v-card-title>
             <v-card-text>
+                <v-form ref="form" v-model="valid" lazy-validation>
                 <v-container grid-list-md @keyup.enter="login">
                     <v-flex xs12>
                         <v-text-field v-model="email" label="이메일" :rules="emailRules" required></v-text-field>
@@ -21,11 +22,12 @@
                         로그인에 성공하였습니다.
                     </v-alert>
                 </v-container>
+                </v-form>
             </v-card-text>
             <v-card-actions class="pb-3">
                 <v-spacer></v-spacer>
                 <v-btn color="blue darken-1" flat @click.native="dialog = false">취소</v-btn>
-                <v-btn color="blue darken-1" flat @click.native="login" :loading="reqLoading">로그인</v-btn>
+                <v-btn :disabled="!valid" color="blue darken-1" flat @click.native="login" :loading="reqLoading">로그인</v-btn>
             </v-card-actions>
         </v-card>
     </v-dialog>
@@ -38,6 +40,7 @@
         name: 'Login',
         data: () => ({
             dialog: false,
+            valid:true,
             reqLoading: false,
 
             loginFail: false,
