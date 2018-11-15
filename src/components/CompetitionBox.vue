05b148beae4cc83f9afb02fb5104f7117e8348f0
Andy0414 <pjh8667@naver.com>
2018-11-15 21:05:47 +0900

    isApplicable bar resize

--- src/components/CompetitionBox.vue
+++ src/components/CompetitionBox.vue
@@ -4,12 +4,12 @@
             <v-card class="pa-2 ma-3" slot-scope="{ hover }" :color='(data.isJoin ? "green darken-4" : "")'>
                 <v-img :src="data.img" aspect-ratio="1">
                     <v-expand-transition>
-                        <div v-if="(hover && data.isApplicable)" class="d-flex transition-fast-in-fast-out green darken-3 v-card--reveal display-1 white--text"
-                            style="height: 20%;">
+                        <div v-if="(hover && data.isApplicable)" class="d-flex transition-fast-in-fast-out green darken-3 title v-card--reveal white--text"
+                            style="height: 30px">
                             신청가능
                         </div>
-                        <div v-if="(hover && !data.isApplicable)" class="d-flex transition-fast-in-fast-out red darken-3 v-card--reveal display-1 white--text"
-                            style="height: 20%;">
+                        <div v-if="(hover && !data.isApplicable)" class="d-flex transition-fast-in-fast-out red darken-3 title v-card--reveal white--text"
+                            style="height: 30px;">
                             신청불가
                         </div>
                     </v-expand-transition>
