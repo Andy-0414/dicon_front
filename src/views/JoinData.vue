cc668f0793f402bf8370a71473c8942a591a1b5c
Andy0414 <pjh8667@naver.com>
2018-12-07 18:53:35 +0900

    profile wordwrap

--- src/views/JoinData.vue
+++ src/views/JoinData.vue
@@ -1,7 +1,7 @@
 <template>
     <v-content class="backColor">
         <v-layout row wrap>
-            <v-flex sm12 md4 lg3>
+            <v-flex lg4 md12>
                 <v-card class="pa-2 ma-3">
                     <v-data-table :headers='header' :items="joinData" class="elevation-1" item-key="email" hide-actions>
                         <template slot="items" slot-scope="props">
@@ -22,8 +22,10 @@
                 <v-card class="pa-3 ma-3" color="rgba(0,0,0,0.1)" v-if="currentUser">
                     <v-card-title>
                         <v-layout align-center justify-center row fill-height>
-                            <v-avatar size="100" color="grey lighten-4" class="ma-5"><v-img :src="getMainPath+'/noneImage.png'" aspect-ratio="1" /></v-avatar>
-                            <v-flex class="display-2">{{currentUser.email}}</v-flex>
+                            <v-flex class="display-2 w">
+                                <v-avatar size="100" color="grey lighten-4" class="ma-5"><v-img :src="getMainPath+'/noneImage.png'" aspect-ratio="1" /></v-avatar>
+                                {{currentUser.email}}
+                                </v-flex>
                         </v-layout>
                     </v-card-title>
                     <v-card v-if="getContestData" flat v-for="(i,index) in getContestData.question" :key="i.label" class="pa-2 ma-3" color="rgba(0,0,0,0.1)">
@@ -84,3 +86,9 @@
         }
     } 
 </script>
+<style scoped>
+    .w{
+        overflow: hidden; 
+        word-wrap: break-word;
+    }
+</style>
